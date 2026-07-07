0c73bb2cc872603f59b0641985b6046a0cfa6670
mariongcl <marion.gic20@gmail.com>
2026-07-07 10:46:34 +0200

    Add WORDS array with translations and descriptions
    Added an array of words with English and French translations along with descriptions.

--- word.js
+++ word.js
@@ -1,1 +1,22 @@
-
+const WORDS = [
+    {
+        en:"apple",
+        fr:"pomme",
+        description:"fruit"
+    },
+    {
+        en:"dog",
+        fr:"chien",
+        description:"animal domestique"
+    },
+    {
+        en:"cat",
+        fr:"chat",
+        description:"animal domestique"
+    },
+    {
+        en:"house",
+        fr:"maison",
+        description:"habitation"
+    }
+];
